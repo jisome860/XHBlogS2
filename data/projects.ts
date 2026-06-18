bdb5b1a7e88266f6914df0dcec43b70f5a319c9e
jisome860 <e876153783@gmail.com>
2026-06-18 21:58:07 +0800

    Auto sync source code for Vercel 🚀

--- data/projects.ts
+++ data/projects.ts
@@ -14,11 +14,11 @@ export const projectsData: Project[] = [
     "id": "proj_1775049332705",
     "name": "Computational Chemistry Tool",
     "githubUrl": "https://github.com/heiehiehi/Computational_Chemistry_Tool",
-    "description": "该工具本作者使用在Win下的WSL2平台，系统为Ubuntu22，个人使用请依据自己数据进行修改（这些工具只是整合了一些流程）",
+    "description": "我没有555，挂一下原作者的：该工具本作者使用在Win下的WSL2平台，系统为Ubuntu22，个人使用请依据自己数据进行修改（这些工具只是整合了一些流程）",
     "icon": "🚀",
     "tags": [
       "Gromacs",
       "RMSF"
     ]
-  },
+  }
 ];
