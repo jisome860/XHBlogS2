66a140d1f0c14feee7d197b6ed6e1d6b3aac6bbc
jisome860 <e876153783@gmail.com>
2026-06-19 17:29:43 +0800

    Auto sync source code for Vercel 🚀

--- data/friends.ts
+++ data/friends.ts
@@ -3,11 +3,27 @@ export interface Friend { id: string; name: string; url: string; description: st
 
 export const friendsData: Friend[] = [
   {
+    "id": "friend_1781861328652",
+    "name": "rwy",
+    "url": "https://blog.wayne.kdns.fr/",
+    "avatar": "https://images.c-o.de5.net/file/1781861269278_5E5457298C91E586F87550E5D1AC7950.png",
+    "description": "wzhzf",
+    "themeColor": "#6366f1"
+  },
+  {
+    "id": "friend_1781861022093",
+    "name": "wzh",
+    "url": "https://personal-website-2je.pages.dev/",
+    "avatar": "https://ts1.tc.mm.bing.net/th/id/R-C.47d7920ccecfe7343db20211ed9c52e5?rik=M8NSDQV1%2f1NPDA&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20200411%2fdbd20b118869495e8b24c2f6c69b91f2.jpeg&ehk=Wq75UFsV81VnGADMV%2f0XOh3DwkRajCxaMg5%2bzACDhX0%3d&risl=&pid=ImgRaw&r=0",
+    "description": "gyhzz",
+    "themeColor": "#6366f1"
+  },
+  {
     "id": "friend_1780933096902",
     "name": "gyh",
     "url": "https://gtang.cc.cd",
     "avatar": "https://images.c-o.de5.net/file/pixiv/1777250194492_20250312_122417_125981673_p0.webp",
-    "description": "wzh之父",
+    "description": "wzhzf",
     "themeColor": "#6366f1"
   },
   {
